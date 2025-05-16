44269e27ea85776dcdf03816ddcb2cd9366332e0
Atharva <sinhaatharva688@gmail.com>
2025-05-16 09:05:47 +0530

    Commit All

--- src/Oops_Concept/Polymorphism/Static_Polymorphism.java
+++ src/Oops_Concept/Polymorphism/Static_Polymorphism.java
@@ -1,4 +1,18 @@
-package Oops_Concept.Polymorphism;
+package Oops_Concept.Polymorphism;/*
+Static Polymorphism (Compile-time polymorphism) in real life says that
+the same action can behave differently depending on the input parameters.
+For example, a Manual car can accelerate by a fixed amount or by a
+specific amount you request. In programming, we achieve this via method
+overloading: multiple methods with the same name but different signatures.
+*/
 
 public class Static_Polymorphism {
+    public static void main(String[] args) {
+        Manual_Car myManualCar = new Manual_Car("Suzuki", "WagonR");
+        myManualCar.startEngine();
+        myManualCar.accelerate();
+        myManualCar.accelerate(40);
+        myManualCar.brake();
+        myManualCar.stopEngine();
+    }
 }
